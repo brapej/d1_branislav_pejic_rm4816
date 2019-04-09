24bf9a48793d7f44ca91eb6ec326f8334815912f
brapej <brapej@yandex.ru>
2019-04-09 21:05:34 +0200

    Prvi domaci, ceo.

---
+++ apps/domaci1/scan.h
@@ -0,0 +1,9 @@
+#ifndef SCAN_H_INCLUDED
+#define SCAN_H_INCLUDED
+
+void load_config(const char *scancodes_filename, const char *mnemonic_filename);
+int process_scancode(int scancode, char *buffer);
+extern char status;
+
+
+#endif
